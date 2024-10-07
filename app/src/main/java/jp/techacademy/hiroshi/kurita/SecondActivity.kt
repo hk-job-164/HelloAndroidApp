419b0b97d92eada29517c18bd02ce15df8180f1c
hk-job-164 <h.kurita.job@gmail.com>
2024-10-07 11:48:51 +0900

    Lesson3 課題後の資料をもとに修正

---
+++ app/src/main/java/jp/techacademy/hiroshi/kurita/SecondActivity.kt
@@ -0,0 +1,21 @@
+package jp.techacademy.hiroshi.kurita
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.techacademy.hiroshi.kurita.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val value1 = intent.getIntExtra("VALUE1", 0)
+        val value2 = intent.getIntExtra("VALUE2", 0)
+
+        binding.textView.text = "${value1 + value2}"
+    }
+}
